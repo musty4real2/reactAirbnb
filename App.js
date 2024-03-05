838ac598f246f327cf384fca0c38f1e8d37cb665
musty4real2 <musty4real2@gmail.com>
2024-03-05 18:36:32 +0100

    first

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 /*
 Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
@@ -7,6 +8,9 @@ Check the Figma file for the design specifics.
 
 export default function App() {
     return (
-        <h1>App component</h1>
+        <div>
+        <Navbar />
+        <Hero />
+    </div>
     )
 }

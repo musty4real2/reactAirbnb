a84d488439a573659fe88c1871e15636da5cff06
musty4real2 <musty4real2@gmail.com>
2024-03-10 20:34:20 +0100

    changes

--- component/Navbar.js
+++ component/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav>
             <img 
-            src="../images/airbnb-logo.png" />
+            src="../images/airbnb-logo.png" className="nav--logo"/>
         </nav>
     )
 }

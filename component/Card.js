8b3db906c8bf40146478d6fcec085b07512d8eb9
musty4real2 <musty4real2@gmail.com>
2024-03-27 14:20:59 +0100

    update

--- component/Card.js
+++ component/Card.js
@@ -3,14 +3,18 @@ import React from "react"
 export default function Card(){
     return(
         <div className="card">
-        <img src="../images/katie-zaferes.png" className="card--image" />
+        <img 
+            src="../images/katie-zaferes.png" 
+            className="card--image" 
+            alt="Image of Katie Zaferes."
+        />
         <div className="card--stats">
             <img src="../images/star.png" className="card--star" />
             <span>5.0</span>
             <span className="gray">(6) • </span>
             <span className="gray">USA</span>
         </div>
-        <p>Life Lessons with Katie Zaferes</p>
+        <h2>Life Lessons with Katie Zaferes</h2>
         <p><span className="bold">From $136</span> / person</p>
     </div>
     )
